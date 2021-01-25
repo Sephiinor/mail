6fd1c07cc4f81554d05bd0c7dcc37a29b069af25
Wx <w229084881.vip.qq.com>
2021-01-25 11:42:04 +0800

    commit

--- src/test/java/com/example/demo/test/MailTest.java
+++ src/test/java/com/example/demo/test/MailTest.java
@@ -21,38 +21,39 @@ import org.thymeleaf.TemplateEngine;
 public class MailTest {
 
     @Autowired
-    private MailService mailService;
+    private MailService    mailService;
     @Autowired
     private TemplateEngine templateEngine;
     @Value("w229084881@vip.qq.com")
-    private String to;
+    private String         to;
 
     @Test
-    public void testSimpleMail(){
-        mailService.sendSimpleMail(to,"simple mail","simple mail has sent");
+    public void testSimpleMail() {
+        mailService.sendSimpleMail(to, "simple mail", "simple mail has sent");
     }
 
     @Test
-    public void testHtmlMail(){
-        String content="<html>\n" +
+    public void testHtmlMail() {
+        String content = "<html>\n" +
                 "<body>\n" +
                 "   <h3>就这样把! 这是一封Html邮件!</h3>\n" +
                 "</body>\n" +
                 "</html>";
-        mailService.sendHtmlMail(to,"html mail",content);
+        mailService.sendHtmlMail(to, "html mail", content);
     }
 
     @Test
-    public void sendAttachmentsMail(){
-        String filePath="E:\\360\\123.txt";
-        mailService.sendAttachmmentsMail(to,"附件邮件","Attachments,receive please ",filePath);
+    public void sendAttachmentsMail() {
+        String filePath = "E:\\360\\123.txt";
+        mailService.sendAttachmmentsMail(to, "附件邮件", "Attachments,receive please ", filePath);
     }
+
     @Test
-    public void sendInlineResourceMail(){
+    public void sendInlineResourceMail() {
         String rscId = "pic001";
         String content = "<html><body>这是有图片的邮件：<img src=\'cid:" + rscId + "\' ></body></html>";
         String imgPath = "E:\\360\\音视频相关话术.png";
 
-        mailService.sendInlineResourceMail(to,"图片邮件",content,imgPath,rscId);
+        mailService.sendInlineResourceMail(to, "图片邮件", content, imgPath, rscId);
     }
 }
